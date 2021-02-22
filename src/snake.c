46a67801f348c06a94230e5896fd87b6d2fadb39
Xavier <xdesilets@pop-os.localdomain>
2021-02-21 20:37:51 -0500

    Implementation of snake-like controls

--- src/snake.c
+++ src/snake.c
@@ -2,34 +2,55 @@
 #include <stdio.h>
 #include <stdbool.h> 
 
-
-void init();
-void checkInput();
-void updateSwitches();
-
 typedef struct {
 	INT8 x;
 	INT8 y;
 }Vector2I;
 
+typedef struct {
+	UINT8 x;
+	UINT8 y;
+}UVector2I;
+
+typedef struct {
+	UVector2I position;
+}Object;
+
 typedef struct{
 	Vector2I movementVector;
+	INT8 movementSpeed;
+	Object object;
 } Player;
 
+unsigned char sprites[] =
+{
+  0x7E,0x7E,0x81,0x81,0xA5,0xA5,0x81,0x81,
+  0x81,0x81,0xBD,0xBD,0x81,0x81,0x7E,0x7E,
+  0x7E,0x7E,0x81,0x81,0xA5,0xA5,0x81,0x81,
+  0x99,0x99,0xA5,0xA5,0x81,0x81,0x7E,0x7E
+};
+
+void init();
+void checkInput();
+void updateSwitches();
+void ApplyPlayerPosition();
+
 Player player;
+
 void main() {
 
 	player.movementVector.x = 0;
 	player.movementVector.y = 0;
+	player.object.position.x = 80;
+	player.object.position.y = 72;
+	player.movementSpeed = 1;
 
-	printf("X value %d \n", player.movementVector.x);
-	printf("Y value %d \n", player.movementVector.y);
-
-	//printf("Position Y: %d \n", MainPlayer->directionVector->y);
 	init();
 	
 	while(1) {
 		checkInput();				// Check for user input (and act on it)
+		ApplyPlayerPosition();
+		move_sprite(0, player.object.position.x, player.object.position.y);
 		updateSwitches();			// Make sure the SHOW_SPRITES and SHOW_BKG switches are on each loop
 		wait_vbl_done();			// Wait until VBLANK to avoid corrupting memory
 	}
@@ -39,18 +60,12 @@ void main() {
 void init() {
 	
 	DISPLAY_ON;						// Turn on the display
-	//set_bkg_data(0, 47, alpha);		// Load 47 tiles into background memory
-	
-	// Use the 'helloWorld' array to write background tiles starting at 0,6 (tile positions)
-	//  and write for 10 tiles in width and 2 tiles in height
-	//set_bkg_tiles(0,6,10,2,helloWorld);
-	
+
 	// Load the the 'sprites' tiles into sprite memory
-	//set_sprite_data(0, 1, sprites);
+	set_sprite_data(0, 1, sprites);
 	
 	// Set the first movable sprite (0) to be the first tile in the sprite memory (0)
-	//set_sprite_tile(0,0);
-
+	set_sprite_tile(0,0);
 }
 
 void updateSwitches() {
@@ -63,37 +78,43 @@ void updateSwitches() {
 
 void checkInput() {
 
-    if (joypad() & J_B) {
-	player.movementVector.x++;
-	player.movementVector.y--;
+	if(joypad() & J_A){
+		player.movementSpeed--;
+	}
+	else if(joypad() & J_B){
+		player.movementSpeed++;
+	}
 
-	printf("X value %d \n", player.movementVector.x);
-	printf("Y value %d \n", player.movementVector.y);
-    }
-	
 	// UP
 	if (joypad() & J_UP) {
-
+		player.movementVector.x = 0 * player.movementSpeed;
+		player.movementVector.y = -1 * player.movementSpeed;
 	}
 
 	// DOWN
-	if (joypad() & J_DOWN) {
-			
+	else if (joypad() & J_DOWN) {
+		player.movementVector.x = 0 * player.movementSpeed;
+		player.movementVector.y = 1 * player.movementSpeed;
 	}
 
 	// LEFT
-	if (joypad() & J_LEFT) {
-		
-		
+	else if (joypad() & J_LEFT) {
+		player.movementVector.x = -1 * player.movementSpeed;
+		player.movementVector.y = 0 * player.movementSpeed;
 	}	
 	
 	// RIGHT
-	if (joypad() & J_RIGHT) {
-		
+	else if (joypad() & J_RIGHT) {
+		player.movementVector.x = 1 * player.movementSpeed;
+		player.movementVector.y = 0 * player.movementSpeed;
 	}
-	
-	// Move the sprite in the first movable sprite list (0)
-	//  the the position of X (player[0]) and y (player[1])
-	//move_sprite(0, player[0], player[1]);
 }
+
+
+void ApplyPlayerPosition(){
+	player.object.position.x += player.movementVector.x;
+	player.object.position.y += player.movementVector.y;
+}
+
+
 
