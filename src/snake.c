7926630150f3cca6bcedc2cb02854712e61da395
Xavier <xdesilets@pop-os.localdomain>
2021-02-22 22:40:20 -0500

    Adding 1 tail to snake

--- src/snake.c
+++ src/snake.c
@@ -6,6 +6,9 @@
 #define TRUE 1
 #define FALSE 0
 #define TAIL_MAX_LENGTH 25
+#define START_TAIL_SPRITE_INDEX 2
+#define PLAYER_SPRITE_INDEX 0
+#define TARGET_SPRITE_INDEX 1
 
 const int SCREEN_X_MIN = 0;
 const int SCREEN_X_MAX = 168;
@@ -23,6 +26,7 @@ typedef struct {
 }UVector2I;
 
 typedef struct {
+	UVector2I lastPosition;
 	UVector2I position;
 	UVector2I size;
 }Object;
@@ -34,7 +38,6 @@ typedef struct{
 } Player;
 
 typedef struct{
-	UVector2I lastPosition;
 	Object object;
 } TailObject;
 
@@ -55,6 +58,8 @@ int checkInput();
 void updateSwitches();
 void ApplyPlayerPosition();
 void SetTargetPositionRandom();
+void MoveSnake();
+void SetObjectPosition(Object *object, UINT8 posX, UINT8 posY);
 UINT8 CollisionCheck(Object* object1, Object* object2);
 
 INT16 randSeed = 0;
@@ -63,6 +68,8 @@ Player player;
 TailObject tail[TAIL_MAX_LENGTH];
 
 Object target;
+
+INT8 tailLength;
 
 void main() {
 	
@@ -77,6 +84,8 @@ void main() {
 	//Initialise target members
 	target.size.x = 8;
 	target.size.y = 8;
+
+	tailLength = 0;
 
 
 	init();
@@ -100,13 +109,13 @@ void main() {
 	while(TRUE) {
 		checkInput();				// Check for user input (and act on it)
 		ApplyPlayerPosition();
-		move_sprite(0, player.object.position.x, player.object.position.y);
-		move_sprite(1, target.position.x, target.position.y);
+		MoveSnake();
 		updateSwitches();			// Make sure the SHOW_SPRITES and SHOW_BKG switches are on each loop
 		wait_vbl_done();			// Wait until VBLANK to avoid corrupting memory
 
 		if(CollisionCheck(&player.object, &target) == TRUE){
 			SetTargetPositionRandom();
+			tailLength++;
 		}
 	}
 	
@@ -117,13 +126,17 @@ void init() {
 	DISPLAY_ON;						// Turn on the display
 
 	// Load the the 'sprites' tiles into sprite memory
-	set_sprite_data(0, 8, PlayerSprite);
+	set_sprite_data(PLAYER_SPRITE_INDEX, 8, PlayerSprite);
 	// Set the first movable sprite (0) to be the first tile in the sprite memory (0)
-	set_sprite_tile(0,0);
+	set_sprite_tile(PLAYER_SPRITE_INDEX,0);
 
-	set_sprite_data(4, 8, TargetSprite);
+	set_sprite_data(TARGET_SPRITE_INDEX, 8, TargetSprite);
 
-	set_sprite_tile(1,1);
+	set_sprite_tile(TARGET_SPRITE_INDEX,1);
+
+	for (int i = 0; i < TAIL_MAX_LENGTH - 1; ++i){
+		set_sprite_tile(i + START_TAIL_SPRITE_INDEX,0);
+	}
 }
 
 void updateSwitches() {
@@ -174,6 +187,9 @@ int checkInput() {
 
 void ApplyPlayerPosition(){
 
+	player.object.lastPosition.x = player.object.position.x;
+	player.object.lastPosition.y = player.object.position.y;
+
 	player.object.position.x += player.movementVector.x;
 	player.object.position.y += player.movementVector.y;
 
@@ -189,6 +205,14 @@ void ApplyPlayerPosition(){
 	else if(player.object.position.y > SCREEN_Y_MAX){
 		player.object.position.y = SCREEN_Y_MIN;
 	}
+}
+
+void SetObjectPosition(Object *object, UINT8 posX, UINT8 posY){
+	object->lastPosition.x = object->position.x;
+	object->lastPosition.y = object->position.y;
+
+	object->position.x = posX;
+	object->position.y = posY;
 }
 
 void SetTargetPositionRandom(){
@@ -208,6 +232,8 @@ void SetTargetPositionRandom(){
 	}
 
 	initrand(target.position.y * target.position.x);
+
+	move_sprite(1, target.position.x, target.position.y);
 }
 
 UINT8 CollisionCheck(Object* object1, Object* object2) {
@@ -225,6 +251,16 @@ UINT8 CollisionCheck(Object* object1, Object* object2) {
 	
 	}
 
+}
+
+void MoveSnake(){
+	move_sprite(0, player.object.position.x, player.object.position.y);
+
+	if(tailLength > 0){
+		SetObjectPosition(&tail[0].object, player.object.lastPosition.x + (8 * -player.movementVector.x), player.object.lastPosition.y + (8 * -player.movementVector.y));
+		move_sprite(START_TAIL_SPRITE_INDEX, tail[0].object.position.x, tail[0].object.position.y);
+	}
+	//for(int i )
 }
 
 
