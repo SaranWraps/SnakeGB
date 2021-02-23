c4091c5742c6b8226440e090e75202b417f97ce0
Xavier <xdesilets@pop-os.localdomain>
2021-02-22 21:48:42 -0500

    Adding snakes logic

--- src/snake.c
+++ src/snake.c
@@ -1,6 +1,11 @@
 #include <gb/gb.h>
 #include <stdio.h>
-#include <stdbool.h> 
+#include <stdlib.h>
+#include <rand.h>
+
+#define TRUE 1
+#define FALSE 0
+#define TAIL_MAX_LENGTH 25
 
 const int SCREEN_X_MIN = 0;
 const int SCREEN_X_MAX = 168;
@@ -28,24 +33,40 @@ typedef struct{
 	Object object;
 } Player;
 
-unsigned char sprites[] =
+typedef struct{
+	UVector2I lastPosition;
+	Object object;
+} TailObject;
+
+unsigned char PlayerSprite[] =
 {
   0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
   0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF
 };
 
+unsigned char TargetSprite[] =
+{
+  0x3C,0x3C,0x7E,0x42,0xC3,0x81,0xC3,0x81,
+  0xC3,0x81,0xC3,0x81,0x7E,0x42,0x3C,0x3C
+};
+
 void init();
-void checkInput();
+int checkInput();
 void updateSwitches();
 void ApplyPlayerPosition();
+void SetTargetPositionRandom();
+UINT8 CollisionCheck(Object* object1, Object* object2);
+
+INT16 randSeed = 0;
 
 Player player;
+TailObject tail[TAIL_MAX_LENGTH];
+
+Object target;
 
 void main() {
-
+	
 	//Initialise Player members
-	player.movementVector.x = 0;
-	player.movementVector.y = 0;
 	player.movementSpeed = 1;
 
 	player.object.position.x = SCREEN_X_MAX / 2;
@@ -53,14 +74,40 @@ void main() {
 	player.object.size.x = 8;
 	player.object.size.y = 8;
 
+	//Initialise target members
+	target.size.x = 8;
+	target.size.y = 8;
+
+
 	init();
-	
-	while(1) {
-		checkInput();				// Check for user input (and act on it)
+
+	//printf("test: %d \n", tail[0].lastPosition.x );
+
+	while (checkInput() == FALSE)
+	{
+		randSeed++; //we increment our seed for our number generator
+
 		ApplyPlayerPosition();
 		move_sprite(0, player.object.position.x, player.object.position.y);
 		updateSwitches();			// Make sure the SHOW_SPRITES and SHOW_BKG switches are on each loop
+		wait_vbl_done();
+	}
+
+	initrand(randSeed);
+
+	SetTargetPositionRandom();
+	
+	while(TRUE) {
+		checkInput();				// Check for user input (and act on it)
+		ApplyPlayerPosition();
+		move_sprite(0, player.object.position.x, player.object.position.y);
+		move_sprite(1, target.position.x, target.position.y);
+		updateSwitches();			// Make sure the SHOW_SPRITES and SHOW_BKG switches are on each loop
 		wait_vbl_done();			// Wait until VBLANK to avoid corrupting memory
+
+		if(CollisionCheck(&player.object, &target) == TRUE){
+			SetTargetPositionRandom();
+		}
 	}
 	
 }
@@ -70,10 +117,13 @@ void init() {
 	DISPLAY_ON;						// Turn on the display
 
 	// Load the the 'sprites' tiles into sprite memory
-	set_sprite_data(0, 1, sprites);
-	
+	set_sprite_data(0, 8, PlayerSprite);
 	// Set the first movable sprite (0) to be the first tile in the sprite memory (0)
 	set_sprite_tile(0,0);
+
+	set_sprite_data(4, 8, TargetSprite);
+
+	set_sprite_tile(1,1);
 }
 
 void updateSwitches() {
@@ -84,38 +134,41 @@ void updateSwitches() {
 	
 }
 
-void checkInput() {
-
-	if(joypad() & J_A){
-		player.movementSpeed--;
-	}
-	else if(joypad() & J_B){
-		player.movementSpeed++;
-	}
+int checkInput() {
 
 	// UP
-	if (joypad() & J_UP) {
-		player.movementVector.x = 0 * player.movementSpeed;
-		player.movementVector.y = -1 * player.movementSpeed;
+	if ((joypad() & J_UP) && player.movementVector.y != 1) {
+		player.movementVector.x = 0;
+		player.movementVector.y = -1;
+
+		return TRUE;
 	}
 
 	// DOWN
-	else if (joypad() & J_DOWN) {
-		player.movementVector.x = 0 * player.movementSpeed;
-		player.movementVector.y = 1 * player.movementSpeed;
+	else if ((joypad() & J_DOWN) && player.movementVector.y != -1) {
+		player.movementVector.x = 0;
+		player.movementVector.y = 1;
+
+		return TRUE;
 	}
 
 	// LEFT
-	else if (joypad() & J_LEFT) {
-		player.movementVector.x = -1 * player.movementSpeed;
-		player.movementVector.y = 0 * player.movementSpeed;
+	else if ((joypad() & J_LEFT) && player.movementVector.x != 1) {
+		player.movementVector.x = -1;
+		player.movementVector.y = 0;
+
+		return TRUE;
 	}	
 	
 	// RIGHT
-	else if (joypad() & J_RIGHT) {
-		player.movementVector.x = 1 * player.movementSpeed;
-		player.movementVector.y = 0 * player.movementSpeed;
+	else if ((joypad() & J_RIGHT) && player.movementVector.x != -1) {
+		player.movementVector.x = 1;
+		player.movementVector.y = 0;
+
+		return TRUE;
 	}
+
+	return FALSE;
 }
 
 
@@ -138,18 +191,37 @@ void ApplyPlayerPosition(){
 	}
 }
 
-UINT8 CollisionCheck(Object object1, Object object2) {
+void SetTargetPositionRandom(){
 
-	if ((object1.position.x < (object2.position.x + object1.size.x)) && 
-		((object1.position.x + object1.size.x) > object2.position.x) && 
-		(object1.position.y < (object2.size.y + object2.position.y)) && 
-		((object1.position.y + object1.size.y) > object2.position.y)) {
+	target.position.x = rand();
+
+	while (target.position.x > SCREEN_X_MAX - 8)
+	{
+		target.position.x /= 2;
+	}
+	
+	target.position.y = rand();
+
+	while (target.position.y > SCREEN_Y_MAX - 8)
+	{
+		target.position.y /= 2;
+	}
+
+	initrand(target.position.y * target.position.x);
+}
+
+UINT8 CollisionCheck(Object* object1, Object* object2) {
+
+	if ((object1->position.x < (object2->position.x + object1->size.x)) && 
+		((object1->position.x + object1->size.x) > object2->position.x) && 
+		(object1->position.y < (object2->size.y + object2->position.y)) && 
+		((object1->position.y + object1->size.y) > object2->position.y)) {
 		
-		return 1;
+		return TRUE;
 	
 	} else {
 		
-		return 0;
+		return FALSE;
 	
 	}
 
